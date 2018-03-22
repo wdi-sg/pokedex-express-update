6326743c132f14974344494fae9705a28848d211
Alethea Toh <alethea.toh@duke.edu>
2018-03-22 22:33:08 +0800

    done

--- index.js
+++ index.js
@@ -58,6 +58,94 @@ app.get('/:id', (request, response) => {
   });
 });
 
+`Expose a new GET /:id/edit endpoint that will return a HTML page with a form for the user to edit an existing pokemon's information`
+app.get('/:id/edit', (request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    if (err) console.error(err);
+
+    // attempt to retrieve the requested pokemon
+    let inputId = request.params.id;
+    let pokemon = obj.pokemon.find((currentPokemon) => {
+      return currentPokemon.id === parseInt(inputId, 10);
+    });
+
+    if (pokemon === undefined) {
+      // return 404 HTML page if pokemon not found
+      response.render('404');
+    } else {
+      // return pokemon HTML page if found
+      let context = {
+        pokemon: pokemon,
+        id: inputId
+      };
+
+      response.render('edit', context);
+    }
+  });
+});
+
+app.put('/:id', (request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    if (err) console.error(err);
+
+    // attempt to retrieve the requested pokemon
+    let inputId = request.params.id;
+    let pokemon = obj.pokemon.find((currentPokemon) => {
+      return currentPokemon.id === parseInt(inputId, 10);
+    });
+
+    if (pokemon === undefined) {
+      // return 404 HTML page if pokemon not found
+      response.render('404');
+    }
+    // Validate each field to ensure only sensible inputs are accepted before saving to pokedex.json (eg. name should not have numbers)
+    else if (! isNaN(request.body.name)) {
+      response.send("invalid input");
+    }
+    else {
+      // UPDATE POKEMON DETAILS and return pokemon HTML page if found
+      if (request.body.name !== "") obj.pokemon[inputId - 1].name = request.body.name;
+      if (request.body.img !== "") obj.pokemon[inputId - 1].img = request.body.img;
+      if (request.body.height !== "") obj.pokemon[inputId - 1].height = request.body.height;
+      if (request.body.weight !== "") obj.pokemon[inputId - 1].weight = request.body.weight;
+
+      jsonfile.writeFile(FILE, obj, {spaces: 4}, function(err2) {
+        if (err2) console.error(err2);
+        // redirect to GET /:id
+
+        response.redirect(`/${request.params.id}`);
+      });
+    }
+  });
+});
+
+app.delete('/:id', (request, response) => {
+  jsonfile.readFile(FILE, (err, obj) => {
+    if (err) console.error(err);
+
+    // attempt to retrieve the requested pokemon
+    let inputId = request.params.id;
+
+    for (let i = 0; i < obj.pokemon.length; i++) {
+      let currentPokemon = obj.pokemon[i];
+
+      if (currentPokemon.id === parseInt(inputId, 10)) {
+        // delete pokedex object
+        obj.pokemon.splice(i,1);
+        break;
+      }
+    }
+
+    jsonfile.writeFile(FILE, obj, {spaces: 4}, function(err2) {
+      if (err2) console.error(err2);
+      // redirect to GET /:id
+
+      response.redirect(`/`);
+    });
+
+  });
+});
+
 app.get('/', (request, response) => {
   jsonfile.readFile(FILE, (err, obj) => {
     if (err) console.error(err);
